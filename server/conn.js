b475484c8658c28a9f50a2af89f89202be5f0666
Nguyen Thuc <lenguyenthuc.edu@gmail.com>
2018-07-17 17:27:39 +0700

    Config connect

--- server/conn.js
+++ server/conn.js
@@ -4,10 +4,14 @@ var connection = mysql.createConnection({
   // user:'ngthuc',
   // password:'ITC@2018',
   // database:'itc_db2018'
-  host:'localhost',
-  user:'root',
-  password:'mysql',
-  database:'nckh'
+  // host:'localhost',
+  // user:'root',
+  // password:'mysql',
+  // database:'nckh'
+  host:'sv.hoangthaibuinguyen.net',
+  user:'admin_nckh',
+  password:'iot@2018',
+  database:'admin_nckh'
 });
 
 // var connection = mysql.createConnection('mysql://ngthuc:tsv2018_26@db4free.net/tsv2018_26?charset=utf8_unicode_ci&timezone=Asia/Ho_Chi_Minh');
