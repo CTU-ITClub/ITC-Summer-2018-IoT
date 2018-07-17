f893f306c82a63fbbe6bcd9175e2f35a69b72452
Nguyen Thuc <lenguyenthuc.edu@gmail.com>
2018-07-17 17:41:56 +0700

    Update README

--- server/models.js
+++ server/models.js
@@ -1,21 +1,6 @@
 var db = require('./conn');
 
-var Model={
-	// getAllCard:function(callback){
-	// 	return db.query("Select * from sinhvien",callback);
-	// },
-	// getCardById:function(id,callback){
-	// 	return db.query("select * from sinhvien where Id=?",[id],callback);
-	// },
-	// addCard:function(sinhvien,callback){
-	// 	return db.query("Insert into sinhvien(name,class,dob) values(?,?,?)",[sinhvien.name,sinhvien.class,sinhvien.dob],callback);
-	// },
-	// deleteCard:function(id,callback){
-	// 	return db.query("delete from sinhvien where Id=?",[id],callback);
-	// },
-	// updateCard:function(id,sinhvien,callback){
-	// 	return db.query("update sinhvien set name=?,class=?,dob=? where Id=?",[sinhvien.name,sinhvien.class,sinhvien.dob,id],callback);
-	// }
+var Model = {
 	getAllCard:function(callback){
 		return db.query("SELECT * FROM card",callback);
 	},
