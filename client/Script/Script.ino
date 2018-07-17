6e59de51b0dd2921abe7609ca4c1cb996c10991d
Hoàng Thái Bùi Nguyễn <hoangthaibmvl@gmail.com>
2018-07-17 17:31:58 +0700

    Code for Client

---
+++ client/Script/Script.ino
@@ -0,0 +1,101 @@
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <RDM6300.h>
+#include <ArduinoJson.h>
+
+//#define ssid  "PKOVER_Wifi"
+//#define pass  "01678911202"
+
+#define RFID_RX_PIN 4
+#define RFID_TX_PIN 1
+#define coi 1         //coi
+#define greenled 16    // Den Xanh
+#define redled 7      // Den Do
+
+int tag;
+char chuoi[10];
+
+
+RDM6300 RFID(RFID_RX_PIN, RFID_TX_PIN);
+
+
+StaticJsonBuffer<256> obj;
+JsonObject& datasv = obj.createObject();
+
+char noichuoi(int tag){
+  return '000'+tag;
+}
+
+
+void beep(){
+  digitalWrite(coi, HIGH);
+  delay(200);
+  digitalWrite(coi, LOW);
+  delay(200);
+  digitalWrite(coi, HIGH);
+  delay(200);
+  digitalWrite(coi, LOW);
+}
+
+void thanhcong(){
+  digitalWrite(greenled, HIGH);
+  delay(200);
+  digitalWrite(greenled, LOW);
+  delay(200);
+  digitalWrite(greenled, HIGH);
+  delay(200);
+  digitalWrite(greenled, LOW);
+}
+
+void thatbai(){
+  digitalWrite(redled, HIGH);
+  delay(200);
+  digitalWrite(redled, LOW);
+  delay(200);
+  digitalWrite(redled, HIGH);
+  delay(200);
+  digitalWrite(redled, LOW);
+}
+ 
+void setup() {
+ 
+  Serial.begin(115200);                                  //Serial connection
+  WiFi.begin("PKOVER_Wifi", "01678911202");   //WiFi connection
+ 
+  while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
+ 
+    delay(500);
+    Serial.println("Waiting for connection");
+ 
+  }
+ Serial.println("WIFI OK");
+}
+ 
+void loop() {
+  if(RFID.isIdAvailable()) {
+   tag = RFID.readId();
+   sprintf(chuoi, "%d", tag);
+   datasv["idCard"]= int (tag);
+ 
+   char JSONmessageBuffer[300];
+    datasv.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+    Serial.println(JSONmessageBuffer);
+   HTTPClient http;    //Declare object of class HTTPClient
+ 
+   http.begin("http://192.168.43.178:10110/card/req");      //Specify request destination
+   http.addHeader("Content-Type", "application/json");  //Specify content-type header
+ 
+    int httpCode = http.POST(JSONmessageBuffer);   //Send the request
+    String payload = http.getString();   
+  
+   Serial.println(httpCode);   //Print HTTP return code
+   Serial.println(payload);    //Print request response payload
+ 
+   http.end();  //Close connection
+   thanhcong();
+   Serial.println("Thanh cong");
+  }
+
+}
+
+
