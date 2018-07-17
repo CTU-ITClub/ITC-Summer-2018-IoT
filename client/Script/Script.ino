6def7ea9eb48866ef2c8d4fe8ca0b4ad74c835a2
Hoàng Thái Bùi Nguyễn <hoangthaibmvl@gmail.com>
2018-07-17 21:37:09 +0700

    Library for Client + Clean Code

--- client/Script/Script.ino
+++ client/Script/Script.ino
@@ -3,8 +3,8 @@
 #include <RDM6300.h>
 #include <ArduinoJson.h>
 
-//#define ssid  "PKOVER_Wifi"
-//#define pass  "01678911202"
+#define ssid  "PKOVER_Wifi"
+#define pass  "01678911202"
 
 #define RFID_RX_PIN 4
 #define RFID_TX_PIN 1
@@ -13,19 +13,13 @@
 #define redled 7      // Den Do
 
 int tag;
-char chuoi[10];
-
+char chuoi[11];
 
 RDM6300 RFID(RFID_RX_PIN, RFID_TX_PIN);
 
 
 StaticJsonBuffer<256> obj;
 JsonObject& datasv = obj.createObject();
-
-char noichuoi(int tag){
-  return '000'+tag;
-}
-
 
 void beep(){
   digitalWrite(coi, HIGH);
@@ -60,7 +54,7 @@ void thatbai(){
 void setup() {
  
   Serial.begin(115200);                                  //Serial connection
-  WiFi.begin("PKOVER_Wifi", "01678911202");   //WiFi connection
+  WiFi.begin(ssid, pass);   //WiFi connection
  
   while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
  
@@ -68,7 +62,7 @@ void setup() {
     Serial.println("Waiting for connection");
  
   }
- Serial.println("WIFI OK");
+ Serial.println("WIFI Connected");
 }
  
 void loop() {
@@ -80,17 +74,16 @@ void loop() {
    char JSONmessageBuffer[300];
     datasv.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
     Serial.println(JSONmessageBuffer);
-   HTTPClient http;    //Declare object of class HTTPClient
- 
-   http.begin("http://192.168.43.178:10110/card/req");      //Specify request destination
-   http.addHeader("Content-Type", "application/json");  //Specify content-type header
- 
+    HTTPClient http;    //Declare object of class HTTPClient
+    
+    http.begin("http://192.168.43.178:10110/card/req");      //Specify request destination
+    http.addHeader("Content-Type", "application/json");  //Specify content-type header
     int httpCode = http.POST(JSONmessageBuffer);   //Send the request
     String payload = http.getString();   
-  
+   
    Serial.println(httpCode);   //Print HTTP return code
    Serial.println(payload);    //Print request response payload
- 
+   
    http.end();  //Close connection
    thanhcong();
    Serial.println("Thanh cong");
